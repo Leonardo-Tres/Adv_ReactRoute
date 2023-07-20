1c4aeaf5ca33aa59ce641fc3b3dd0cb86245fae5
Leonardo-Tres <leonardotres1@gmail.com>
2023-07-20 16:51:27 +1000

    Actually this is the final commit

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,5 +1,10 @@
+import PageContent from "../components/PageContent"
+
 const HomePage = () => {
-    return <h1>HomePage</h1>
+    return <PageContent>
+        <h1>Welcome!</h1>
+        <p>Browse to find out our amazing events</p>
+    </PageContent>
 }
 
 export default HomePage
